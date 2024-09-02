9c022250b75cb96b47098f39c210d40f37df01cd
host <taijomi17@gmail.com>
2024-09-02 14:44:25 -0400

    config variables env y providers para formularios

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:teslo_shop/config/config.dart';
-import 'package:teslo_shop/config/router/app_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'config/config.dart';
+
+Future<void> main() async {
+  await Environment.initEnvironment();
+
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
